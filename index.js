64dbcee2d05b4cf19043d3661d9b6862a16c70ed
Nycholas Marques <nycholasantonny@gmail.com>
2023-04-24 23:56:32 -0300

    portfolio quase finalizado, responsivo criado
    faltando somente adicionar textos, no mais, responsivo, javascript e todas as seções criadas, faltando somente adicionar textos em geral sobre minha trajetória até aqui  \o/

--- index.js
+++ index.js
@@ -10,3 +10,50 @@ toggleBtn.onclick = function () {
 
     toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars"
 }
+
+// skills
+let websites = document.querySelector('.website-skill');
+let games = document.querySelector('.games-skill')
+let websiteContainer = document.querySelector('.projects-container')
+let gamesContainer = document.querySelector('.games-projects');
+let websiteBtns = document.querySelectorAll('.projectBtn');
+
+websites.addEventListener('click', function() {
+    websites.classList.add('activePs');
+    games.classList.remove('activePs');
+    websiteContainer.style.display = 'grid'
+    gamesContainer.style.display = 'none';
+
+    websiteBtns.forEach(function(button) {
+        button.style.visibility = 'visible';
+        button.style.transition = 'none'
+    });
+  });
+  
+  games.addEventListener('click', function() {
+    games.classList.add('activePs');
+    websites.classList.remove('activePs');
+    gamesContainer.style.display = 'block'
+    if (games.classList.contains('activePs')) {
+        websiteContainer.style.display = 'none';
+        gamesContainer.style.display = 'block'
+        setTimeout(function() {
+            websiteBtns.forEach(function(button) {
+                button.style.visibility = 'hidden';
+                button.style.transition = 'none'
+            });
+        }, 100);
+    }
+  });
+
+// card
+
+let cardBtns = document.querySelectorAll('.projectBtn')
+
+cardBtns.forEach((cardBtn) =>
+    cardBtn.addEventListener('click', () => {
+        let cardProject = cardBtn.closest('.project-card');
+         cardProject.classList.toggle('active');
+    }
+))
+  
